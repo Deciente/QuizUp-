f3cdbc7e68ac1b80bfc07e4275c61dc76243639b
Deciente <jack.tianhaowang@gmail.com>
2016-01-14 21:51:10 -0500

    First Commit

---
+++ QuizUp2/app/src/main/java/jackwang/quizup/Question.java
@@ -0,0 +1,30 @@
+package jackwang.quizup;
+
+/**
+ * Created by thwang on 12/5/15.
+ */
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue){
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
